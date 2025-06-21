b573215d13eefb011958f9e85c5f0538227770f8
PKUMAR0-DEV <prince.rudra95@gmail.com>
2025-06-21 12:04:04 +0530

    Update main.tf

--- main.tf
+++ main.tf
@@ -1,3 +1,25 @@
+provider "azurerm" {
+  features {
+    resource_group {
+      prevent_deletion_if_contains_resources = false
+    }
+  }
+
+
+  subscription_id = var.subscription_id
+}
+# provider "azurerm" {                        
+#   features {}
+  
+#   subscription_id = var.subscription_id
+ 
+# }
+provider "kubernetes" {
+  config_path = "~/.kube/config"
+}
+//az aks get-credentials --resource-group aks-resources-kkt-9955 --name example-aks1
+
+
 resource "azurerm_resource_group" "example" {
   name     = "aks-resources-kkt-9955"
   location = "West Europe"
