de44995530a882725049b2b774bda5e2641526d4
PKUMAR0-DEV <prince.rudra95@gmail.com>
2025-06-21 12:40:12 +0530

    Update info.tf

--- info.tf
+++ info.tf
@@ -1,1 +1,9 @@
+#az ad sp create-for-rbac --name "my-sp" --role contributor --scopes /subscriptions/375e3e31-22f5-413c-85ee-c3f44db2f249 --sdk-auth
 
+
+#apt-get update && apt-get install curl -y
+#apt-get install iputils-ping
+
+
+#az aks get-credentials --resource-group aks-resources-kkt-9955 --name example-aks1
+#kubectl create namespace argo-rollouts && kubectl config set-context --current --namespace=argo-rollouts
